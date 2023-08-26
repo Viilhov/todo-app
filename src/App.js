20435e70cb504901f1de510b536b6c90e3b011b9
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-08-26 14:35:22 +0200

    Added form component and styled the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Header from "./components/Header";
+import Form from "./components/Form";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
       <Header />
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import styles from "../style.module.css";
+
+const Form = ({ todo, setTodo, todoList, setTodoList }) => {
+  const handleChange = (event) => {
+    setTodo(event.target.value);
+    console.log(todo);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodoList([...todoList, todo]);
+    console.log(todoList);
+  };
+  return (
+    <div className={styles.todoform}>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={todo}
+          onChange={handleChange}
+          className={styles.todoinput}
+          placeholder="Add todo item"
+        ></input>
+        <button type="submit" className={styles.todobutton}>
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Form;
